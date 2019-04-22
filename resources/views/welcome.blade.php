974c0723100d2de49a08a04254cecc42c962bbd2
mylaravelgit <1021513892@qq.com>
2019-04-22 09:49:47 +0800

    heard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,274 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<!-- saved from url=(0022)http://wap.bh0531.com/ -->
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>大好时光</title>
+    <meta name="keywords" content="大好时光-开公司|公司注册|注册公司">
+    <meta name="description" content="大好时光-开公司|公司注册|注册公司">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="财务之家">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="stylesheet" href="./cwzj/index.css">
+    <script type="text/javascript" charset="utf-8" src="./cwzj/JS_Float.aspx"></script>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+</head>
+<body>
+<div class="layout">
+    <header class="layout_header" id="layout_header">
+        <div class="action_bar">
+            <a class="list" id="toggle-btn" href="javascript:;">
+            </a>
+            <span class="app_title">
+		    	  	 </span>
+            <a href="tel:400-850-0074" class="phone">
+            </a>
+        </div>
+        <ul class="collapse" >
+            <li class="first">
+                <a onclick="openZoosUrl(&#39;chatwin&#39;);" href="javascript:void(0);">首页</a>
+            </li>
+            <li>
+                <a onclick="openZoosUrl(&#39;chatwin&#39;);" href="javascript:void(0);">联系我们</a>
+            </li>
+        </ul>
+    </header>
+    <div class="layout_body">
+        <div class="banner">
+            <img src="./cwzj/banner.png" onclick="openZoosUrl(&#39;chatwin&#39;);">
+        </div>
+        <div class="row1">
+            <p class="title">
+                真正极简 快捷流程
+            </p>
+            <p class="sub_title">
+                综合化管理 只需三步 完成注册
+            </p>
+            <div class="con">
+                <div>
+                    <img src="./cwzj/row1_i1.png">
+                    <p>免费查询</p>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div>
+                    <img src="./cwzj/row1_i2.png">
+                    <p>人工核实</p>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div>
+                    <img src="./cwzj/row1_i3.png">
+                    <p>极速响应</p>
+                </div>
+            </div>
+            <div class="fn-clear"></div>
+            <a class="go_btn" onclick="openZoosUrl(&#39;chatwin&#39;);" href="javascript:void(0);">点击咨询 查询能否注册</a>
+        </div>
+        <div class="row2">
+            <img src="./cwzj/row2_i1.png">
+        </div>
+        <div class="row3">
+            <p class="title">
+                注册公司前不了解<br>
+                会有哪些烦心事？
+            </p>
+            <div class="con fn-clear">
+                <div class="row_to fn-clear">
+                    <div>
+                        <div class="step">1</div>
+                        办理时间长
+                    </div>
+                    <div>
+                        <div class="step">2</div>
+                        多跑冤枉路
+                    </div>
+                    <div>
+                        <div class="step">3</div>
+                        注册效率低
+                    </div>
+                    <div>
+                        <div class="step">4</div>
+                        多花冤枉钱
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+        <div class="row4">
+            <p class="title">
+                五大优势 金牌保障
+            </p>
+            <p class="sub_title">
+                实力说明一切
+            </p>
+            <img src="./cwzj/row4_i1.png">
+            <a class="go_btn" onclick="openZoosUrl(&#39;chatwin&#39;);" href="javascript:void(0);">点击咨询 了解注册费用</a>
+        </div>
+
+        <div class="row6">
+            <p class="title">
+                每<span class="b_c">1小时</span>就有一家公司<br>
+                在财务之家选择一站式注册服务
+            </p>
+            <img src="./cwzj/row6_i1.png">
+            <div class="con">
+                <div class="con_c">
+                    为您提供高性价比<br>
+                    一站式注册服务
+                </div>
+            </div>
+        </div>
+        <div class="row7">
+            <p class="title">
+                财务之家公司注册流程
+            </p>
+            <p class="sub_title">
+                一站式服务 意在满足您的各项需求
+            </p>
+            <ul>
+                <div class="step">
+                    <div class="main">
+                        <img src="./cwzj/row7_i1.png">
+                    </div>
+                    <div class="con">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <li>确认合作</li>
+                <li>核名通知书</li>
+                <li>工商注册</li>
+                <li>刻章</li>
+                <li>国、地税报到</li>
+                <li>次月开始代账</li>
+                <div class="fn-clear"></div>
+            </ul>
+            <div class="fn-clear"></div>
+        </div>
+        <div class="row8">
+            <p class="title">
+                您可能还需要的服务
+            </p>
+            <p class="sub_title">
+                一站式服务 意在满足您的各项需求
+            </p>
+            <div class="con fn-clear">
+                <div>
+                    <p>
+                        <img src="./cwzj/row8_i1.png">
+                    </p>
+                    企业代理记账
+                </div>
+                <div>
+                    <p>
+                        <img src="./cwzj/row8_i2.png">
+                    </p>
+                    商标注册服务
+                </div>
+                <div>
+                    <p>
+                        <img src="./cwzj/row8_i3.png">
+                    </p>
+                    申请一般纳税人
+                </div>
+                <div>
+                    <p>
+                        <img src="./cwzj/row8_i4.png">
+                    </p>
+                    汇算清缴报告
+                </div>
+            </div>
+            <a onclick="openZoosUrl(&#39;chatwin&#39;);" href="javascript:void(0);" class="go_btn">点击咨询 代理记账服务</a>
+        </div>
+
+        <div class="row10">
+            <p class="title">
+                媒体报道
+            </p>
+            <div class="con fn-clear">
+                <div>
+                    <p>低于市场同类注册公司30%以上价格......</p>
+                </div>
+                <div>
+                    <p>注册公司市场非常混乱，没有一个标准，财务之家竖立行业标杆......</p>
+                </div>
+                <div>
+                    <p>解决年轻人创业问题。我们不做跟风狗。只做高性价比的服务。哪怕市场再小......</p>
+                </div>
+                <div>
+                    <p>让靠谱的创业方服务为一群优秀的年轻人服务......</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="footer_tel">
+            <img src="./cwzj/footer_tel.png">
+        </div>
+        <div class="last_ foot_bottom_left" style="color: rgba(255, 255, 255, 0.0980392);">Powered by 大好时光（佛山市）企业管理有限公司
+            粤ICP备18159108号
+        </div>
+    </footer>
+</div>
+<script type="text/javascript" src="./cwzj/jquery-2.1.1.min.js"></script>
+
+
+
+
+<script language="javascript" src="./cwzj/LsJS.aspx"></script>
+<link href="./cwzj/JS5.css" rel="stylesheet" type="text/css">
+
+<div id="LRdiv1" style="display:none;"></div>
+<style>
+    @-webkit-keyframes moveToBottom {
+        from {
+        }
+        to {
+            -webkit-transform: translateY(100%);
+        }
+    }
+
+    @keyframes moveToBottom {
+        from {
+        }
+        to {
+            -webkit-transform: translateY(100%);
+            transform: translateY(100%);
+        }
+    }
+
+    .arrow-up {
+        height: auto;
+        width: 25px;
+        position: absolute;
+        left: 50%;
+        bottom: 6%;
+        margin-left: -12px;
+        z-index: 9999;
+    }
+
+    .pt-page-moveIconUp {
+        -webkit-animation: moveIconUp ease 1.5s both infinite;
+        animation: moveToBottom ease 1.5s both infinite;
+    }
+</style>
+
+<script language="javascript" src="./cwzj/LsJS.aspx"></script>
+<script>
+    $(document).ready(function () {
+        $("#toggle-btn").click(function () {
+            $(".collapse").slideToggle();
+        })
+
+    })
+</script>
+
+
+</body>
 </html>
